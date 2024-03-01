2da4e01f7ba22a848411797d8c48a0b19d943bdd
William <william@edu9e53.kent.ac.uk>
2024-03-01 05:33:02 +0000

    adding files that I added to already

---
+++ init.lua
@@ -0,0 +1,40 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+--video stopped at 12.40
+
+
+local opts = {}
+require("lazy").setup("plugins")
+
+vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left<CR>")
+
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"lua", "javascript", "python", "java"},
+  highlight = { enabled = true},
+  indent = { enable = true},
+  })
+
+
+
+require("gruvbox").setup()
+vim.cmd.colorscheme "gruvbox"
+
