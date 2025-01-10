1b45052435d469299d2590a7150f377523770b18
chcknlegwill <willn70@outlook.com>
2025-01-10 22:13:10 +0000

    Changing to LunarVim for a little bit as that has less issues with the lsp configuration.

---
+++ config.lua
@@ -0,0 +1,31 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+--
+
+
+lvim.leader = "space"
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+Terminal",
+  f = { "<cmd>ToggleTerm<cr>", "Floating terminal" },
+  v = { "<cmd>2ToggleTerm size=30 direction=vertical<cr>", "Split vertical" },
+  h = { "<cmd>2ToggleTerm size=30 direction=horizontal<cr>", "Split horizontal" },
+}
+
+lvim.colorscheme = "gruvbox"
+
+-- going to install gruvbox colourscheme
+--
+
+
+
+lvim.plugins = {
+  { "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = true,
+    opts = ...},
+
+}
+
