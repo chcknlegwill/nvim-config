2436fff1f6c968e6efff011447c0632f6c612241
chcknlegwill <willn70@outlook.com>
2025-01-11 05:26:51 +0000

    Going to use lunarvim as it is MUCH better than my current config and I can edit it pretty easily.

--- config.lua
+++ config.lua
@@ -3,8 +3,7 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
---
-
+vim.opt.clipboard="unnamedplus"
 
 lvim.leader = "space"
 lvim.builtin.which_key.mappings["t"] = {
@@ -17,15 +16,13 @@ lvim.builtin.which_key.mappings["t"] = {
 lvim.colorscheme = "gruvbox"
 
 -- going to install gruvbox colourscheme
---
-
 
 
 lvim.plugins = {
   { "ellisonleao/gruvbox.nvim",
     priority = 1000,
     config = true,
-    opts = ...},
-
+    opts = {},
+  },
 }
 
