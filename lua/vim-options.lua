8f213707602836f0c421d2d0c864582e4a89fdbb
William <WillN70@outlook.com>
2025-05-14 16:57:13 +0100

    added fine-cmdline - will fix error

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,3 +5,8 @@ vim.cmd("set number")
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.g.mapleader = " "
 
+vim.keymap.set("n", ":", ":", { desc = "Open default command-line" })
+
+-- buffer management-- buffer management-- buffer management
+vim.keymap.set("n", "<leader>bn", ":bnext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<leader>bp", ":bprevious<CR>", { desc = "Previous buffer" })
